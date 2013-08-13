3092ed506b9fc82f1a3ef63b04584ab503982b31
Sergio García Mondaray <sgmonda@gmail.com>
2013-08-13 12:02:40 +0200

    Browser backspace behavoir fix

--- public/js/core.js
+++ public/js/core.js
@@ -91,3 +91,26 @@ loadJS(['/js/settings.js', '/socket.io/socket.io.js'], function(){
     });
 
 });
+
+// #############################################################################
+// ## BROWSER SETTINGS #########################################################
+// #############################################################################
+
+// Prevent backspace from going back
+$(document).unbind('keydown').bind('keydown', function (event) {
+    var doPrevent = false;
+    if (event.keyCode === 8) {
+        var d = event.srcElement || event.target;
+        if ((d.tagName.toUpperCase() === 'INPUT' && (d.type.toUpperCase() === 'TEXT' || d.type.toUpperCase() === 'PASSWORD' || d.type.toUpperCase() === 'FILE')) 
+             || d.tagName.toUpperCase() === 'TEXTAREA') {
+            doPrevent = d.readOnly || d.disabled;
+        }
+        else {
+            doPrevent = true;
+        }
+    }
+
+    if (doPrevent) {
+        event.preventDefault();
+    }
+});
